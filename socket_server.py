21846faa133f8c9869f5479cc80fdd7d00c029d7
root <root@localhost.localdomain>
2016-01-02 20:44:57 -0800

    Create port, Bind Port & IP, Set server to listen

--- socket_server.py
+++ socket_server.py
@@ -6,4 +6,12 @@ srv=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #Get IP
 srv_ip=socket.gethostbyname(socket.gethostname())
 
+#Define Port
+srv_port=1111
+
+#Bind Port & IP
+srv.bind((srv_ip,srv_port))
+
+#Listen from potential clients #argument contains max number of queued conn
+srv.listen(2)
 
